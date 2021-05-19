736b4d3b7b47ead90be331c329e7869bfeff1cad
Alex Hadida <aaahadida@gmail.com>
2021-05-19 21:16:28 +0200

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -1,24 +1,24 @@
 
 module.exports.run = async (client, message, args, options) => {
 
-    var guildIDData = options.active.get(message.guild.id);
+    // var guildIDData = options.active.get(message.guild.id);
 
-    if (!guildIDData) return message.channel.send("❌**Nothing playing in this server.**");
+    // if (!guildIDData) return message.channel.send("❌**Nothing playing in this server.**");
 
-    if (message.member.voice.channel != message.guild.me.voice.channel) return message.channel.send("You aren't in the same voice channel.");
+    // if (message.member.voice.channel != message.guild.me.voice.channel) return message.channel.send("You aren't in the same voice channel.");
 
-    if (isNaN(args[0]) || args[0] > 150 || args[0] < 0) return message.reply("Choose a number between 0 - 150.");
+    // if (isNaN(args[0]) || args[0] > 150 || args[0] < 0) return message.reply("Choose a number between 0 - 150.");
 
-    guildIDData.dispatcher.setVolume(args[0] / 100);
+    // guildIDData.dispatcher.setVolume(args[0] / 100);
 
-    return message.channel.send(`🔊 Volume changed to ${args[0]}`);
+    // return message.channel.send(`🔊 Volume changed to ${args[0]}`);
 
 }
 
 module.exports.help = {
     name: "volume",
-    description: "Change the volume of the bot.",
-    category: "Music",
+    description: "",
+    category: "",
     aliases: ["geluid"]
 }
 
