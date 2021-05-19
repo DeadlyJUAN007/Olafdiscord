b4728b44beaab16459df7c7cb18f77b40d86598b
Alex Hadida <aaahadida@gmail.com>
2021-05-19 21:35:06 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,15 +2,15 @@ module.exports.run = async (client, message, args) => {
 
     // var sendTime = message.createdTimestamp;
 
-    var time = message.createdTimestamp - Date.now();
+    // var time = message.createdTimestamp - Date.now();
 
     // return message.channel.send(`🏓Pong: ${message.createdTimestamp - Date.now()}ms`);
 
     // return message.channel.send(`🏓Pong: ${sendTime}ms`);
 
-    // return message.channel.send("🏓Pong:" + (message.createdTimestamp - Date.now()) +  " ms");
+    return message.channel.send("🏓Pong:" + (message.createdTimestamp - Date.now()) + " ms");
 
-    return message.channel.send(`🏓Pong: ${time}ms`);
+    // return message.channel.send(`🏓Pong: ${time}ms`);
 }
 
 module.exports.help = {
